eb93e26dcb9177c615f6a3097621572ed30b95e2
beepboup <aysha.braybrooke@etu.unilim.fr>
2023-10-26 15:18:22 +0200

    Update script.js

---
+++ dist/script.js
@@ -0,0 +1,25 @@
+// function toggleIcon() {
+//     const label = document.querySelector('label[for="menuburger"]');
+//     if (label.classList.contains('iconMenu')) {
+//       label.classList.remove('iconMenu');
+//       label.classList.add('iconX');
+//     } else {
+//       label.classList.remove('iconX');
+//       label.classList.add('iconMenu');
+//     }
+//   }
+
+
+// document.querySelector('.menuburger').addEventListener('click', function () {
+//     document.querySelector('input[type="checkbox"]').checked = !document.querySelector('input[type="checkbox"]').checked;
+//   });
+  
+//   document.querySelector('.label2').addEventListener('click', function () {
+//     document.querySelector('input[type="checkbox"]').checked = !document.querySelector('input[type="checkbox"]').checked;
+//   });
+
+
+// document.querySelector('.header__label').addEventListener('click', function () {
+//     const checkbox = document.querySelector('input[type="checkbox"]');
+//     checkbox.checked = !checkbox.checked;
+//   });
